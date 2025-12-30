06052bf6654a71e3a7cf1c8ea161ef8f1093d853
puck.shen <puck.shen@quectel.com>
2025-12-30 20:30:00 +0100

    v4 dynamicallywq

--- demo_chr.c
+++ demo_chr.c
@@ -1,9 +1,17 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h> 
 
 #define DEVICE_NAME "hello_char"
+#define CLASS_NAME "hello_class"
+
 static int major;
+static struct class *hello_class = NULL;
+static struct device *hello_device = NULL;
+static dev_t dev_num;
+static struct cdev hello_cdev;  
 
 static ssize_t hello_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
 {
@@ -12,24 +20,78 @@ static ssize_t hello_read(struct file *file, char __user *buf, size_t count, lof
 }
 
 static struct file_operations fops ={
+    .owner = THIS_MODULE,
     .read = hello_read,
 };
 
 static int __init hello_init(void)
 {
+    int retval;
     pr_info("Hello from kernel module! \n");
-    major = register_chrdev(0, DEVICE_NAME, &fops);
-    if (major < 0) {
-        pr_err("Failed to register char device\n");
-        return major;
+
+    //1- Dynamically allocate major device number
+    retval = alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME);
+    if (retval < 0) {
+        pr_err("Failed to allocate char device region\n");
+        return retval;
     }
+
+    major = MAJOR(dev_num);
     pr_info("Hello char major number: %d\n", major);
+
+    //2- Initialize and add cdev structure
+    cdev_init(&hello_cdev, &fops);    
+    hello_cdev.owner = THIS_MODULE;
+    
+    retval = cdev_add(&hello_cdev, dev_num, 1);
+    if (retval < 0) {
+        pr_err("Failed to add cdev\n");
+        unregister_chrdev_region(dev_num, 1);
+        return retval;
+    }
+
+    //3- Create class
+    hello_class = class_create(THIS_MODULE ,CLASS_NAME);
+    if (IS_ERR(hello_class)) {
+        unregister_chrdev_region(dev_num, 1);
+        pr_err("Failed to create device class\n");
+        return PTR_ERR(hello_class);
+    }
+
+    //4- Create device node
+    hello_device = device_create(hello_class, NULL, dev_num, NULL, DEVICE_NAME);
+    if (IS_ERR(hello_device)) {
+        class_destroy(hello_class);
+        unregister_chrdev_region(dev_num, 1);
+        pr_err("Failed to create device\n");
+        return PTR_ERR(hello_device);
+    }
+
+    pr_info("Hello char device created successfully\n");
+    pr_info("Device node will be: /dev/%s\n", DEVICE_NAME);
+
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    unregister_chrdev(major, DEVICE_NAME);
+    // Clean the device -> class -> device_num
+    if (hello_device) {
+        device_destroy(hello_class, dev_num);
+        pr_info("device_destroy!\n");
+    }
+    
+    if (hello_class) {
+        class_destroy(hello_class);
+        pr_info("class_destroy!\n");
+    }
+
+    cdev_del(&hello_cdev);  
+    pr_info("cdev_del!\n");
+
+    unregister_chrdev_region(dev_num, 1);
+    pr_info("unregister_chrdev_region!\n");
+
     pr_info("Bye from kernel module!\n");
 }
 
@@ -38,4 +100,4 @@ module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("puck");
-MODULE_DESCRIPTION("Stage3: Char Device with file_operations");
+MODULE_DESCRIPTION("Stage 4: Dynamically allocate major device number");
