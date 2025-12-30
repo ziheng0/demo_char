e5ad13a4eca75ab406339b9cd3bf7155e6b97527
puck.shen <puck.shen@quectel.com>
2025-12-30 19:38:17 +0100

    register chrdev

--- demo_chr.c
+++ demo_chr.c
@@ -1,14 +1,28 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+
+#define DEVICE_NAME "demo_chr"
+static int major;
+
+static struct file_operations fops ={
+};
 
 static int __init hello_init(void)
 {
     pr_info("Hello from kernel module! \n");
+    major = register_chrdev(0, DEVICE_NAME, &fops);
+    if (major < 0) {
+        pr_err("Failed to register char device\n");
+        return major;
+    }
+    pr_info("Hello char major number: %d\n", major);
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
+    unregister_chrdev(major, DEVICE_NAME);
     pr_info("Bye from kernel module!\n");
 }
 
