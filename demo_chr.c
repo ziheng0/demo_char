5157fd6eeb09c4b665c79f7ae2d12b8d41f762b5
puck.shen <puck.shen@quectel.com>
2025-12-30 19:48:44 +0100

    v3 hello_read

--- demo_chr.c
+++ demo_chr.c
@@ -2,10 +2,17 @@
 #include <linux/init.h>
 #include <linux/fs.h>
 
-#define DEVICE_NAME "demo_chr"
+#define DEVICE_NAME "hello_char"
 static int major;
 
+static ssize_t hello_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
+{
+    pr_info("hello_char: read called\n");
+    return 0;
+}
+
 static struct file_operations fops ={
+    .read = hello_read,
 };
 
 static int __init hello_init(void)
@@ -31,4 +38,4 @@ module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("puck");
-MODULE_DESCRIPTION("Hello Kernel Module");
+MODULE_DESCRIPTION("Stage3: Char Device with file_operations");
